623653cda27cbd4c7fdcb80d303140fe08a00595
GowthamL <gowtham_l@outlook.com>
2025-09-02 12:17:35 +0530

    feat: Add initial structure for Build Action DocType and related files

---
+++ actions/actions/doctype/build_action/build_action.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, gowtham and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Build Action", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ actions/actions/doctype/build_action/test_build_action.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, gowtham and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBuildAction(FrappeTestCase):
+	pass
