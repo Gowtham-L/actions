bcb4ae26bad4d2e45ded74881fbc5641d5dffb7a
GowthamL <gowtham_l@outlook.com>
2025-09-02 12:34:04 +0530

    feat: Add full name field and update before_save logic in Build Action DocType

--- actions/actions/doctype/build_action/build_action.py
+++ actions/actions/doctype/build_action/build_action.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, gowtham and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BuildAction(Document):
-	pass
+	def before_save(self):
+		#self.full_name = f"{self.first_name} {self.last_name}"
+		if self.last_name:
+			# Set full name before saving the document
+			self.full_name = f"{self.first_name} {self.last_name}"
+		else: 
+			self.full_name = self.first_name
--- actions/actions/doctype/build_action/test_build_action.py
+++ actions/actions/doctype/build_action/test_build_action.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, gowtham and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestBuildAction(FrappeTestCase):
-	pass
+	def test_full_name_correctly_set(self):
+		test_driver = frappe.new_doc("Build Action")
+		test_driver.first_name = "John"
+		test_driver.last_name = "Doe"
+		test_driver.license_number = "DL123456"
+		test_driver.save()
+
+		self.assertEqual(test_driver.full_name, "John Doe")
